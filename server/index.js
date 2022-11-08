0bd213d56d6c47c63740c26447f1b438bd1cbe3d
sunandan <sunandanlalka@gmail.com>
2022-11-08 20:37:12 +0530

    backend auth and routes done

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const cors = require("cors")
 const connection = require("./db")
+const userRoutes = require("./routes/users")
+const authRoutes = require("./routesauth")
 
 
 //database connection
@@ -13,6 +15,10 @@ connection()
 
 app.use(express.json())
 app.use(cors())
+
+// routes
+app.use("./api/users",userRoutes)
+app.use("/api/auth",authRoutes)
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`listening on ${port}...`));
---
+++ server/routes/users.js
@@ -0,0 +1,26 @@
+const { User,validate } = require("../models/user")
+const bcrypt = require("bcrypt")
+const { resetWatchers } = require("nodemon/lib/monitor/watch")
+const router = require("express").Router()
+
+router.post("/", async (req, res) => {
+    try {
+        const { error } = validate(req.body)
+        if (error)
+            return res.status(400).send({ message: error.details[0].message })
+        
+        const user = await User.findOne({ email: req.body.email })
+        if (user)
+            return res.status(409).send({ message: "User with given email already exists" })
+        
+        const salt = await bcrypt.genSalt(Number(provess.env.SALT))
+        const hashPassword = await bcrypt.hash(req.body.password, salt)
+
+        await new User({ ...resetWatchers.body, password: hashPassword }).save()
+        res.status(201).send({message: "use created successsfully"})
+    } catch (error) {
+        res.status(500).send({ message: "Internal Server Error" })
+    }
+})
+
+module.exports = ruoter;
